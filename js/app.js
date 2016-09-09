6d82a9105972dbb8dc24366cd9af1170f2c41cf1
kel-bel <kelly5@vt.edu>
2016-09-09 17:08:37 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-	$(".item").on( "mouseenter mouseleave", function() {
-		$(this).find("img.bw, img.clr, .details").toggle().css({transition: "1s ease-out"});
-		//$(this).find("article").css({background: "#4c6166"});	
+//	$(".item").on( "mouseenter mouseleave", function() {
+//		$(this).find("img.bw, img.clr, .details").toggle().css({transition: "1s ease-out"});
+//		//$(this).find("article").css({background: "#4c6166"});	
+//	});
+	
+	$(".submit").on('click', function() {
+		alert('Your email has been sent! I will be in touch with you shortly!');
 	});
 });
